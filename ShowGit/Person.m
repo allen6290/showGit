cf3ce8a335593aadc02754297d16d1b3f573b196
htang <hu.tang@yeepay.com>
2015-11-14 23:49:25 +0800

    添加Person头文件和实现类

---
+++ ShowGit/Person.m
@@ -0,0 +1,22 @@
+//
+//  Person.m
+//  ShowGit
+//
+//  Created by 唐琥 on 15/11/14.
+//  Copyright © 2015年 com.yeepay.mobile. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+
+- (void) sayHello:(NSString *) str {
+    
+    
+    NSLog(@"THIS IS THE FIRST HELLO FROM %@",str);
+    
+}
+
+
+@end
