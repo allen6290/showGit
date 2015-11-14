1305b82882ad3970e0033a7eb0f2e46279b7cfa4
htang <hu.tang@yeepay.com>
2015-11-14 23:01:29 +0800

    Initial Commit

---
+++ ShowGit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ShowGit
+//
+//  Created by 唐琥 on 15/11/14.
+//  Copyright © 2015年 com.yeepay.mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
